fa16b419eeba2371e3ff4b049b47d638997f7811
Sara Ahmad <asara1114@gmail.com>
2018-07-23 14:43:39 -0400

    added the ui elements

--- BioSignatures/Login/View/LoginViewController.swift
+++ BioSignatures/Login/View/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Material
+import SnapKit
 
 class LoginViewController: UIViewController {
     
@@ -18,46 +19,94 @@ class LoginViewController: UIViewController {
     }
     
     private lazy var emailTextField: UITextField = {
-        let textfield = 
-        
+        let textfield = UITextField()
+        let emailIcon = UIImageView(image: #imageLiteral(resourceName: "envelope"))
+        textfield.leftView = emailIcon
+        textfield.leftViewMode = .always
+        textfield.dividerContentEdgeInsets.left = .leastNonzeroMagnitude
+        textfield.dividerThickness = 1.0
+        textfield.placeholder = "Email"
+        return textfield
     }()
     
     private lazy var passwordTextField: UITextField = {
-        
-        
+        let textfield = UITextField()
+        let passwordIcon = UIImageView(image: #imageLiteral(resourceName: "key"))
+        textfield.leftView = passwordIcon
+        textfield.leftViewMode = .always
+        textfield.dividerContentEdgeInsets.left = .leastNonzeroMagnitude
+        textfield.dividerThickness = 1.0
+        textfield.placeholder = "Password"
+        return textfield
     }()
     
+    private lazy var textFieldStackView: UIStackView = {
+        var stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+    } ()
+    
     private lazy var toggle: UISwitch = {
-        
+        let toggle = UISwitch()
+        return toggle
     }()
     
     private lazy var toggleLabel: UILabel = {
-        
+        var label = UILabel()
+        label.text = "Add Face ID"
+        return label
+    }()
+    
+    private lazy var addFaceStackView: UIStackView = {
+        var stackView = UIStackView(arrangedSubviews: [toggle, toggleLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
+    } ()
+    
+    private lazy var signatureImageView: UIImageView = {
+        var signatureImgV = UIImageView()
+        signatureImgV.isHidden = true
+        return signatureImgV
     }()
     
     private lazy var addSignatureButton: UIButton = {
-        let button = UIButton.self
+        var button = UIButton()
+        button.addTarget(self, action: #selector(addIndividualSignature), for: .touchUpInside)
+        button.titleLabel?.text = "Add Signature"
         return button
     }()
     
+    private lazy var mainStackview: UIStackView = {
+        var stackView = UIStackView(arrangedSubviews: [textFieldStackView, signatureImageView, addSignatureButton])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+    } ()
+    
     private lazy var createAccountButton: UIButton = {
-        let
+        var button = UIButton()
+        button.titleLabel?.text = "Create Account"
+        return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        view.backgroundColor = .red
-        
         setupViews()
     }
     
     private func setupViews() {
-        
+        view.addSubview(mainStackview)
+        view.addSubview(createAccountButton)
     }
     
+    //private methods
     
+    @objc func addIndividualSignature() {
+        
+        signatureImageView.isHidden = false
+    }
 
  
     //MARK: Set up views
@@ -76,6 +125,8 @@ class LoginViewController: UIViewController {
     
     
     private func setupRegularConstraints() {
+        
+        
         
     }
 
