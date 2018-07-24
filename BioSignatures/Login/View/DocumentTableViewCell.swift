168b9d2dce348ff2582e72f81572f5a04dd541af
Sara Ahmad <asara1114@gmail.com>
2018-07-23 23:50:07 -0400

    document cell

--- BioSignatures/Login/View/DocumentTableViewCell.swift
+++ BioSignatures/Login/View/DocumentTableViewCell.swift
@@ -7,15 +7,88 @@
 //
 
 import UIKit
+import SnapKit
 
 class DocumentTableViewCell: UITableViewCell {
     
+    //top label
+    private lazy var topLabel: UILabel = {
+      let label = UILabel()
+      label.text = "NDA Sign Off Needed"
+        label.font = UIFont.boldSystemFont(ofSize: 14.0)
+      return label
+    } ()
     
+    //second label
+    private lazy var assignedToLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Assigned to Billy Johnson"
+        return label
+    } ()
+    
+    //third label
+    private lazy var timestamp: UILabel = {
+        let label = UILabel()
+        return label
+    } ()
+    
+    //stackView
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [topLabel, assignedToLabel, timestamp])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        return stackView
+    } ()
+    
+    //chevron image
+    private lazy var chevronImg: UIImageView = {
+        let chevron = UIImageView(image: #imageLiteral(resourceName: "right-chevron"))
+        return chevron
+    } ()
+    
+    private lazy var checkImage: UIImageView = {
+        let check = UIImageView(image: #imageLiteral(resourceName: "Checkmark"))
+        check.isHidden = true
+        return check
+    } ()
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
     }
+    
+    private func setup () {
+        setupViews()
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(mainStackView)
+        contentView.addSubview(chevronImg)
+        contentView.addSubview(checkImage)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        guard traitCollection != previousTraitCollection else { return }
+        
+        switch traitCollection.verticalSizeClass {
+        case .regular: setupRegularConstraints()
+        default: break
+        }
+        
+    }
+    
+    
+    private func setupRegularConstraints() {
+        
+        
+    }
+    
+    
 
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
